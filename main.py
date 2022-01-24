e827c1f77e42d577c88085845a0ab7340f64d2f6
AMK <a>
2022-01-25 00:13:04 +0100

    First lesson

---
+++ main.py
@@ -0,0 +1,22 @@
+def dodawanie():
+    liczba_pierwsza = input('First number')
+    liczba_druga = input('Second number')
+    if liczba_pierwsza.isnumeric() and liczba_druga.isnumeric():
+        suma = int(liczba_pierwsza) + int(liczba_druga)
+        return suma
+    else:
+        print('VALUE ERROR!')
+        return False
+
+
+sumaPierwsza = dodawanie()
+sumaDruga = dodawanie()
+
+print(f"Wyniki działań to: {sumaPierwsza} oraz: {sumaDruga}")
+print("Wyniki działań to: "+str(sumaPierwsza)+" oraz: "+str(sumaDruga))
+
+sumaDruga = ()
+sumaTrzecia = {}
+
+print(type(sumaDruga))
+print(type(sumaTrzecia))
