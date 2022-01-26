91232896c1138e077f9a3cd42166544b9c820ce0
AMK <a>
2022-01-26 20:59:28 +0100

    Second lesson

--- 2_SecondLesson_lists.py
+++ 2_SecondLesson_lists.py
@@ -1,8 +1,8 @@
-# Tworzymy pustą listę
+# Tworzymy pustą listę.
 myList = []
 
-# Tworzymy przykładową listę zawierającą dane
-# Zwróć uwagę na fakt, iż lista mozę zawierać różne typy danych. Elementem listy może być inna lista
+# Tworzymy przykładową listę zawierającą dane.
+# Zwróć uwagę na fakt, iż lista mozę zawierać różne typy danych. Elementem listy może być inna lista.
 
 myList = [133, 2, 'XYZ', [3, 4, 'ABC']]
 
@@ -13,34 +13,34 @@ print(f"Cała lista: {myList}")
 print(f"Czwarty element listy jest listą: {myList[3]}")
 print(f"Czwarty element listy i jego trzecia pozycja: {myList[3][2]}")
 
-# zamiast liczb dodatnich możemy także użyć ujemnych
-# nazywa się to negative indexing i pozwala uzyskać dostęp do elementów listy licząc od końca
+# Zamiast liczb dodatnich możemy także użyć ujemnych.
+# Nazywa się to negative indexing i pozwala uzyskać dostęp do elementów listy licząc od końca.
 
 print(f"Ostatni element naszej listy ma indeks -1 {myList[-1]}")
 
-# Każdą listę można pokroić na plasterki, to działanie nazywa się slicing
-# Możemy wyciągnąć fragment naszej listy jak poniżej
-# W nawiasach kwadratowych podajemy pierwszy lelement fragmentu i po dwukropku ostatni
+# Każdą listę można pokroić na plasterki, to działanie nazywa się slicing.
+# Możemy wyciągnąć fragment naszej listy jak poniżej.
+# W nawiasach kwadratowych podajemy pierwszy lelement fragmentu i po dwukropku ostatni.
 # UWAGA!
-# Ostatni element nie będzie zawarty w nowej liście
-# Oznacza to, ze jeżeli chcemy pobrać 2 elementy, poczynając od drugiego, musimy poprosić o indeksy [1:3]
-# Zwrócone zostaną indeksy 1 i 2, indeks numer 3 nie zostanie zwrócony
+# Ostatni element nie będzie zawarty w nowej liście.
+# Oznacza to, ze jeżeli chcemy pobrać 2 elementy, poczynając od drugiego, musimy poprosić o indeksy [1:3].
+# Zwrócone zostaną indeksy 1 i 2, indeks numer 3 nie zostanie zwrócony.
 
 print(f"Zwracamy 2 elementy listy, poczynając od drugiego: {myList[1:3]}")
 
-# Elementy listy można zmieniać
-# Zmieniamy pierwszy element listy
+# Elementy listy można zmieniać.
+# Zmieniamy pierwszy element listy.
 
 myList[0] = 521
 print(f"Nowa lista po zmianie pierwszego elementu na 521: {myList}")
 
-# Tak jak w przypadku slicingu, możemy zmienić fragment listy
+# Tak jak w przypadku slicingu, możemy zmienić fragment listy.
 
 myList[1:3] = ['CSS', [9, 8, 7, 'DDD']]
 print(myList)
 
-# Do listy możemy dodawać nowe elementy. Metody append i extend
-# Dodajemy jedną pozycję
+# Do listy możemy dodawać nowe elementy. Metody append i extend.
+# Dodajemy jedną pozycję.
 
 myList.append(11)
 print(f"Lsta po dodaniu nowego elementu: {myList}")
@@ -50,7 +50,7 @@ print(f"Lsta po dodaniu nowego elementu: {myList}")
 myList.append('ZXC')
 print(f"Lsta po dodaniu nowego elementu: {myList}")
 
-# Aby dodać do listy klilka elementów używamy metody extend
+# Aby dodać do listy klilka elementów używamy metody extend.
 
 myList.extend([11, '11', 234])
 print(f"Nowa lista po dodaniu kliku elementów: {myList}")
@@ -70,7 +70,7 @@ print(f"Dodane 100 na pozycji 2: {myList}")
 myList[3:3] = [999, 998, 997, 996]
 print(f"Dodane 4 pozycje od pozycji 3: {myList}")
 
-# Kopiowanie, shallowcopy i deepcopy
+# Kopiowanie, shallowcopy i deepcopy.
 
 print()
 print(f"Lista przed zmianą: {myList}")
@@ -85,7 +85,23 @@ print(f"Głęboka kopia:      {deepCopy}")
 # print(id(deepCopy))
 print()
 
-# Usuwanie elementów listy
+# Długość listy
+print(f"Aktualna lista: {myList}")
+print(f"Nasza lista zawiera {len(myList)} elementów.")
+print()
+
+print(*range(len(myList)))
+
+for element in range(len(myList)):
+    print(element, " ", myList[element])
+
+# for element in range(len(myList)):
+#     print(element, end=" ")
+#     print(myList[element])
+
+# print([list((element, myList[element])) for element in range(len(myList))])
+
+# Usuwanie elementów listy.
 # Metoda pop usuwa jeden indeks z listy i zwraca jesgo wartość. Argumentem jest indeks.
 # Metoda remove uzuwa jeden element z listy. Argumentem jest wartość.
 # Słowo kluczowe del. Indeks jako argument. Usuwa wiele pozycji lub całą listę.
@@ -99,5 +115,6 @@ del myList[1:3]
 print(f"Usunięty dwa element listy - del:                   {myList}")
 myList.clear()
 print(myList)
+print(len(myList))
 del myList
 # print(myList)
